3f693b3e44b3d17fe6582d260585e04dabf07232
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-06-28 19:41:07 -0300

    Add: Summary

--- Control/RequestControl.cs
+++ Control/RequestControl.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,11 @@ namespace HttpRequestHelper.Control
 {
     class RequestControl
     {
+        /// <summary>
+        /// Generic get method
+        /// </summary>
+        /// <param name="link">Link</param>
+        /// <returns>Response from request.</returns>
         internal static async Task<object> GetAsync<T>(string link)
         {
             HttpClient client = new()
@@ -25,7 +31,15 @@ namespace HttpRequestHelper.Control
                     : null;
         }
 
-        internal static async Task<object> PutAsync<T>(object obj, string link, Encoding encoding, string mediaType, int statusCodeFailure)
+        /// <summary>
+        /// Generic put method
+        /// </summary>
+        /// <param name="obj">Object to send</param>
+        /// <param name="link">Link</param>
+        /// <param name="encoding">Encoding</param>
+        /// <param name="mediaType">Media type</param>
+        /// <returns>Response from request.</returns>
+        internal static async Task<HttpStatusCode> PutAsync<T>(object obj, string link, Encoding encoding, string mediaType)
         {
             HttpClient client = new()
             {
@@ -39,21 +53,34 @@ namespace HttpRequestHelper.Control
             return response.StatusCode;
         }
 
-        internal static async Task<object> PostAsync<T>(object obj, string link, Encoding encoding, string mediaType, int statusCodeFailure)
+        /// <summary>
+        /// Generic post method
+        /// </summary>
+        /// <param name="obj">Object to send</param>
+        /// <param name="link">Link</param>
+        /// <param name="encoding">Encoding</param>
+        /// <param name="mediaType">Media type</param>
+        /// <returns>Response from request.</returns>
+        internal static async Task<HttpStatusCode> PostAsync<T>(object obj, string link, Encoding encoding, string mediaType)
         {
             HttpClient client = new()
             {
                 BaseAddress = new Uri(link)
             };
 
-            JObject document = JObject.FromObject(obj);
-            StringContent content = new(JsonConvert.SerializeObject(document), encoding, mediaType);
-
-            using HttpResponseMessage response = await client.PostAsync(link, content);
+            using HttpResponseMessage response = await client
+                .PostAsync(
+                    link, 
+                    new StringContent(JsonConvert.SerializeObject(JObject.FromObject(obj)), encoding, mediaType));
             return response.StatusCode;
         }
 
-        internal static async Task<object> DeleteAsync<T>(string link)
+        /// <summary>
+        /// Generic delete method
+        /// </summary>
+        /// <param name="link">Link</param>
+        /// <returns>Response from request.</returns>
+        internal static async Task<HttpStatusCode> DeleteAsync<T>(string link)
         {
             HttpClient client = new()
             {
