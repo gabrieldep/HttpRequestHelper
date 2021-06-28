3a090ca75abff4bb13d4a723691359cea022f7da
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-06-28 19:51:03 -0300

    Removido using's desnecessários

--- Control/RequestControl.cs
+++ Control/RequestControl.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
