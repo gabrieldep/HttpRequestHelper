af2dc99b89e674573a0e9a27961550d9d3b30aa9
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-06-28 19:27:54 -0300

    Add: method Get generic

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HttpRequestHelper
-{
-    public class Class1
-    {
-    }
-}
---
+++ Control/RequestControl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestHelper.Control
+{
+    class RequestControl
+    {
+        internal static async Task<object> GetAsync<T>(string link)
+        {
+            HttpClient client = new()
+            {
+                BaseAddress = new Uri(link)
+            };
+
+            using HttpResponseMessage response = await client.GetAsync(link);
+            return response.IsSuccessStatusCode ?
+                JsonConvert.DeserializeObject<T>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult())
+                    : null;
+        }
+    }
+}
