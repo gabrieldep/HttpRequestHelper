638f4641f5b549b2b459d91d97303f8ceee30275
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-07-02 11:41:52 -0300

    GetStatusCode from exception

---
+++ Control/CatchStatusCodeControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestHelper.Control
+{
+    class CatchStatusCodeControl
+    {
+        public static HttpStatusCode GetStatusCode(Exception exception)
+        {
+            Type exceptionType = exception.GetType();
+
+            if(exceptionType == typeof(ArgumentNullException).GetType())
+            {
+                return HttpStatusCode.NoContent;
+            }
+            else if (exceptionType == typeof(TimeoutException).GetType())
+            {
+                return HttpStatusCode.GatewayTimeout;
+            }
+            else
+            {
+                return HttpStatusCode.InternalServerError;
+            }
+
+        }
+    }
+}
