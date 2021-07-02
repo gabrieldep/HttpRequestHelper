d92c26e94d951a808b7d505788da0b1affe68760
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-07-02 11:58:40 -0300

    Add: Retorno para token errado

---
+++ Control/VerificationTokenControl.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestHelper.Control
+{
+    class VerificationTokenControl
+    {
+        public static IActionResult WrongToken()
+        {
+            return new ContentResult
+            {
+                StatusCode = (int)HttpStatusCode.Unauthorized,
+                ContentType = "application/json",
+                Content = JsonConvert.SerializeObject(new { mesasge = "Unauthorized " })
+            };
+        }
+    }
+}
